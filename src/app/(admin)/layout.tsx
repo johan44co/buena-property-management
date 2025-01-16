40ab6cff3f703d13824832b1f0d65c6fb5249938
Johan <johan44co@gmail.com>
2025-01-16 13:04:55 +0100

    Refactor admin layout and introduce breadcrumb navigation in template component

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,18 +1,5 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
@@ -30,26 +17,7 @@ export default async function Layout({
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">Placeholder</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Placeholder</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        {children}
-      </SidebarInset>
+      <SidebarInset>{children}</SidebarInset>
     </SidebarProvider>
   );
 }
---
+++ src/app/(admin)/template.tsx
@@ -0,0 +1,78 @@
+"use client";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
+import { getProperty } from "@/util/property";
+import { useParams, usePathname } from "next/navigation";
+import React from "react";
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const params = useParams();
+  const parts = pathname.split("/").filter(Boolean);
+
+  const [breadcrumb, setBreadcrumb] = React.useState<
+    { name?: string; href: string }[]
+  >([]);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    Promise.all(
+      parts.map(async (part, index) => {
+        if (parts[index - 1] === "properties" && part === params.id) {
+          const propertyName = (await getProperty(params.id)).property?.name;
+          return {
+            name: propertyName,
+            href: `/${parts.slice(0, index + 1).join("/")}`,
+          };
+        }
+        return {
+          name: part,
+          href: `/${parts.slice(0, index + 1).join("/")}`,
+        };
+      }),
+    )
+      .then(setBreadcrumb)
+      .finally(() => setLoading(false));
+  }, [pathname, params.id]);
+
+  return (
+    <>
+      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <div className="flex items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              {loading && (
+                <BreadcrumbItem>
+                  <BreadcrumbLink>
+                    <Skeleton className="h-4 w-48" />
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              )}
+              {breadcrumb.map(({ name, href }, index) => (
+                <React.Fragment key={href}>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink className="capitalize" href={href}>
+                      {name}
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  {index < breadcrumb.length - 1 && <BreadcrumbSeparator />}
+                </React.Fragment>
+              ))}
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </header>
+      {children}
+    </>
+  );
+}
